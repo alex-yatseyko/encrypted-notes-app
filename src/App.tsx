d27356f2051e548be640eb026587e87d9a2d78a0
mac <alex.yatseiko@gmail.com>
2023-03-20 14:47:07 -0400

    7 Saving Notes

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,36 @@
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
 import { Note } from "./types";
-import { storage } from "./storage";
+import storage from "./storage";
 
 import styles from "./App.module.css";
 import { NoteEditor } from "./NoteEditor";
 import { JSONContent } from "@tiptap/react";
 
+const STORAGE_KEY = "notes";
+
+const loadNotes = () => {
+  const noteIds = storage.get<string[]>(STORAGE_KEY, []);
+  const notes: Record<string, Note> = {};
+  noteIds.forEach((id) => {
+    const note = storage.get<Note>(`${STORAGE_KEY}:${id}`);
+    notes[note.id] = {
+      ...note,
+      updatedAt: new Date(note.updatedAt),
+    };
+  });
+  return notes;
+};
+
+const saveNote = (note: Note) => {
+  const noteIds = storage.get<string[]>(STORAGE_KEY, []);
+  const noteIdsWithoutNode = noteIds.filter((id) => id !== note.id);
+  storage.set(STORAGE_KEY, [...noteIdsWithoutNode, note.id]);
+  storage.set(`${STORAGE_KEY}:${note.id}`, note);
+};
+
 function App() {
-  const [notes, setNotes] = useState<Record<string, Note>>({});
+  const [notes, setNotes] = useState<Record<string, Note>>(() => loadNotes());
   const [activeNoteId, setActiveNoteId] = useState<string | null>(null);
 
   const activeNote = activeNoteId ? notes[activeNoteId] : null;
@@ -43,6 +65,7 @@ function App() {
       [newNote.id]: newNote,
     }));
     setActiveNoteId(newNote.id);
+    saveNote(newNote);
   };
 
   const handleChangeActiveNote = (id: string) => {
